4f9365563b22ae7cbe3bc1302caf029d1368679b
maki <maki@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-25 09:03:04 +0000

    drop tags/opMessagePlugin-0.8.2(wrong version...)

--- apps/mobile_frontend/modules/message/templates/_unreadMessage.php
+++
@@ -1,3 +0,0 @@
-<?php if ($unreadMessageCount > 0): ?>
-<?php echo link_to(__('There are new %d messages!', array('%d' => $unreadMessageCount)), '@receiveList') ?><br>
-<?php endif; ?>
--- apps/pc_frontend/modules/message/templates/sendToFriendInput.php
+++
@@ -1,8 +0,0 @@
-<?php
-$options = array('form' => array($form));
-$title = __('Compose Message');
-$options['url'] = 'message/sendToFriend';
-$options['button'] = __('Send');
-$options['isMultipart'] = true;
-include_box('formMessage', $title, '', $options);
-?>
