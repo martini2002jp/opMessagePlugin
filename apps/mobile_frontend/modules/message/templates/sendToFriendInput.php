67955e7eea3ac8f049ff239d345158dc75b4f27f
Maki Takahashi <martini2002jp@gmail.com>
2012-02-15 13:37:25 +0900

    (fixes #1058) added "<br>" between "To:" and "Title" in mobile/sendToFriendInput.php

--- apps/mobile_frontend/modules/message/templates/sendToFriendInput.php
+++ apps/mobile_frontend/modules/message/templates/sendToFriendInput.php
@@ -1,7 +1,8 @@
+<?php use_helper('opMessage') ?>
 <?php op_mobile_page_title(__('Compose Message')) ?>
 <?php if ($sendMember): ?>
 <?php echo __('To') ?>:
-<?php echo link_to($sendMember->getName(), 'member/profile?id='.$sendMember->getId()) ?>
+<?php echo op_message_link_to_member($sendMember) ?><br /><br />
 <?php endif; ?>
 <?php echo $form->renderFormTag(url_for('message/sendToFriend'), array('method' => 'POST')) ?>
 <?php echo $form ?>
