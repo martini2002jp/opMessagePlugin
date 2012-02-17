4294ed8b65ac3aa5ff219190bfe66fb5f3161f57
Maki Takahashi <takahashi@tejimaya.com>
2012-02-17 23:16:16 +0900

    (fixes #2460) fixed configurations in sendToFriend for mobile coloring

--- apps/mobile_frontend/modules/message/templates/sendToFriendInput.php
+++ apps/mobile_frontend/modules/message/templates/sendToFriendInput.php
@@ -1,7 +1,7 @@
 <?php use_helper('opMessage') ?>
 <?php op_mobile_page_title(__('Compose Message')) ?>
 <?php if ($sendMember): ?>
-<?php echo __('To') ?>:
+<font color="<?php echo $op_color['core_color_19'] ?>"><?php echo __('To') ?>:</font>
 <?php echo op_message_link_to_member($sendMember) ?><br /><br />
 <?php endif; ?>
 <?php echo $form->renderFormTag(url_for('message/sendToFriend'), array('method' => 'POST')) ?>
