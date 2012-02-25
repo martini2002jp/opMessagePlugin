2b78f042c553755b47f768c211161465d23c977b
Maki Takahashi <takahashi@tejimaya.com>
2012-02-25 11:43:20 +0900

    (fixes #1058) changed <br /> to <br> in mobile/sendToFriendInput.php

--- apps/mobile_frontend/modules/message/templates/sendToFriendInput.php
+++ apps/mobile_frontend/modules/message/templates/sendToFriendInput.php
@@ -2,7 +2,7 @@
 <?php op_mobile_page_title(__('Compose Message')) ?>
 <?php if ($sendMember): ?>
 <font color="<?php echo $op_color['core_color_19'] ?>"><?php echo __('To') ?>:</font>
-<?php echo op_message_link_to_member($sendMember) ?><br /><br />
+<?php echo op_message_link_to_member($sendMember) ?><br><br>
 <?php endif; ?>
 <?php echo $form->renderFormTag(url_for('message/sendToFriend'), array('method' => 'POST')) ?>
 <?php echo $form ?>
