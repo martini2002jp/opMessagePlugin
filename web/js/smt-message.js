2c7df017cb5de6835ce8e54b724a617c383947cc
urabe <urabe@tejimaya.com>
2013-12-25 17:40:22 +0900

    (refs #3518) update JS for date line. and change function not use global.

--- web/js/smt-message.js
+++ web/js/smt-message.js
@@ -1,4 +1,136 @@
-$(function() {
+'use strict';
+$(document).ready(function() {
+
+  var
+   /**
+    * message wrapper.
+    */
+    $messageWrapper = $('#message-wrapper-parent'),
+
+   /**
+    * message functions.
+    */
+    func = {
+
+      /**
+       * hide and show submit fields.
+       */
+      submitFilter: function() {
+        $('#loading').toggle();
+        $('#submit-message').toggle();
+        $('#do-submit').toggle();
+        $('#message_image').toggle();
+      },
+
+      /**
+       * hide and show more info fields.
+       */
+      moreFilter: function() {
+        $('#loading-more').toggle();
+        $('#more').toggle();
+      },
+
+      /**
+       * get FromData Object. openpne apyKey and form value.
+       */
+      getFormData: function(form)
+      {
+        var
+          formData = new FormData(form[0]);
+
+        formData.append('apiKey', openpne.apiKey);
+
+        $(form.serializeArray()).each(function(i, v) {
+          formData.append(v.name, v.value);
+        });
+
+        return formData;
+      }
+    };
+
+  $messageWrapper.param = {
+
+   /**
+    * partner member id.
+    */
+    memberId: memberId,
+
+   /**
+    * message template.
+    */
+    $template: $('#message-template'),
+  };
+
+  $messageWrapper.func = {
+
+    /**
+     * insert Message template by data.
+     * @param data
+     * @param isAddRow
+     */
+    insertMessageTemplate: function(data, isAddRow) {
+      var
+        template = $messageWrapper.param.$template.children().clone(),
+        $timeInfo = template.find('.time-info'),
+        $timeInfoWrapper = $timeInfo.parent('.time-info-wrapper'),
+        $popoverTitle = template.find('.popover-title'),
+        $messageBody = template.find('.message-body'),
+        $photo = template.find('.photo'),
+        $messageCreatedAt = template.find('.message-created-at'),
+        position = data.member.id == this.memberId ? 'right' : 'left';
+
+      template.attr('data-message-id', data.id);
+      template.addClass(position);
+      template.addClass('show');
+
+      $timeInfo.append(data.formatted_date);
+      $timeInfoWrapper.attr('data-created-at-date', data.created_at_date);
+
+      $popoverTitle.append(data.member.name);
+      $messageBody.append(data.body);
+
+      // has one image data from api. this opMessagePlugin version.
+      if (data.image_path && data.image_tag) {
+        $photo.append('<li><a href="' + data.image_path + '">' + data.image_tag + '</a></li>');
+      } else {
+        $photo.remove();
+      }
+
+      $messageCreatedAt
+        .addClass(position)
+        .attr('title', data.created_at)
+        .timeago();
+
+      isAddRow ? $messageWrapper.append(template) : $messageWrapper.prepend(template);
+    },
+
+    /**
+     * update Time info line.
+     */
+    updateTimeInfo: function() {
+      var
+        timeInfoWrapper = $messageWrapper.find('.time-info-wrapper'),
+        currentDate,
+        baseDate;
+
+      if (timeInfoWrapper.length) {
+        for (var i = 0; i < timeInfoWrapper.length; i++) {
+          currentDate = timeInfoWrapper.eq(i).attr('data-created-at-date');
+          if (currentDate) {
+            if (currentDate === baseDate) {
+              timeInfoWrapper.eq(i).hide();
+            } else {
+              timeInfoWrapper.eq(i).show();
+            }
+
+            baseDate = currentDate;
+          }
+        }
+      }
+    }
+  };
+
+  $messageWrapper.func.updateTimeInfo();
   $('.message-created-at').timeago();
 
   $('#do-submit').click(function() {
@@ -7,28 +139,24 @@ $(function() {
     {
       return;
     }
-    submitFilter();
+    func.submitFilter();
 
-    var form = $('form');
-    var fd = new FormData(form[0]);
-    var json = getParams();
-
-    for (i in json)
-    {
-      fd.append(i, json[i]);
-    }
+    var
+      form = $('form#send-message-form'),
+      formData = func.getFormData(form);
 
     $.ajax({
       url: openpne.apiBase + "message/post.json",
       type: 'POST',
       processData: false,
       contentType: false,
-      data: fd,
+      data: formData,
       dataType: 'json',
       success: function(res) {
         if ('success' === res.status)
         {
-          $('#message-wrapper-parent').append(addTemplate(res.data, memberId));
+          $messageWrapper.func.insertMessageTemplate(res.data, true);
+          $messageWrapper.func.updateTimeInfo();
 
           $('#no-message').hide();
         }
@@ -39,28 +167,29 @@ $(function() {
       complete: function() {
         $('#submit-message').val('');
         $('#message_image').val('');
-        submitFilter();
+        func.submitFilter();
       }
     });
   });
 
   $('#more').click(function() {
-    moreFilter();
+    var
+      firstMessageWrapper = $('.message-wrapper:first'),
+      maxId = -1;
 
-    var maxId = -1;
-    $('.message-wrapper').each(function() {
-      if (0 > maxId || maxId > parseInt($(this).attr('data-message-id')))
-      {
-        maxId = parseInt($(this).attr('data-message-id'));
-      }
-    });
+    func.moreFilter();
+
+    if (firstMessageWrapper)
+    {
+      maxId = parseInt(firstMessageWrapper.attr('data-message-id'));
+    }
 
     $.ajax({
       url: openpne.apiBase + "message/search.json",
       type: 'GET',
       data: {
         apiKey: openpne.apiKey,
-        memberId: memberId,
+        memberId: $messageWrapper.param.memberId,
         maxId: maxId
       },
       dataType: 'json',
@@ -69,10 +198,12 @@ $(function() {
         {
           for (var i = 0; i < res.data.length; i++)
           {
-            $('#message-wrapper-parent').prepend(addTemplate(res.data[i], memberId));
+            $messageWrapper.func.insertMessageTemplate(res.data[i], false);
           }
 
-          moreFilter();
+          $messageWrapper.func.updateTimeInfo();
+
+          func.moreFilter();
         }
 
         if (25 > res.data.length)
@@ -100,54 +231,3 @@ $(function() {
     }
   });
 });
-
-function submitFilter()
-{
-  $('#loading').toggle();
-  $('#submit-message').toggle();
-  $('#do-submit').toggle();
-  $('#message_image').toggle();
-}
-
-function moreFilter()
-{
-  $('#loading-more').toggle();
-  $('#more').toggle();
-}
-
-function getParams()
-{
-  var query = $('form').serializeArray(),
-    json = {apiKey: openpne.apiKey};
-  for (i in query)
-  {
-     json[query[i].name] = query[i].value
-  }
-
-  json['toMember'] = $('#do-submit').attr('to-member');
-
-  return json;
-}
-
-function addTemplate(data, memberId)
-{
-  var template = $('#message-template').children().clone();
-  template.find('.member-image').attr('src', data.member.profile_image);
-  template.find('.popover-title').append(data.member.name);
-  template.find('.message-body').append(data.body);
-  template.attr('data-message-id', data.id);
-  if (data.image_path && data.image_tag)
-  {
-    template.find('.photo').append('<li><a href="' + data.image_path + '">' + data.image_tag + '</a></li>');
-  }
-  else
-  {
-    template.find('.photo').remove();
-  }
-  var position = data.member.id == memberId ? 'right' : 'left';
-  template.find('.message-created-at').addClass(position).attr('title', data.created_at).timeago();
-  template.addClass(position);
-  template.addClass('show');
-
-  return template;
-}
