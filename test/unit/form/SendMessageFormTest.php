8d9e35f02b706e24ec771953f14ad5218c900ae1
Maki Takahashi <takahashi@tejimaya.com>
2014-02-06 17:40:20 +0900

    added test pattern (refs #2744)

--- test/unit/form/SendMessageFormTest.php
+++ test/unit/form/SendMessageFormTest.php
@@ -11,3 +11,16 @@ $replyMessage->setReturnMessageId(5);
 $form = new SendMessageForm($replyMessage);
 $t->is($form->getDefault('subject'), 'Re:test subject');
 $t->is($form->getDefault('body'), "> test body\n> test line");
+
+$replyMessage = new SendMessageData();
+$replyMessage->setReturnMessageId(6);
+
+$form = new SendMessageForm($replyMessage);
+$t->is($form->getDefault('subject'), 'Re:');
+$t->is($form->getDefault('body'), "");
+
+$sendMessage = new SendMessageData();
+
+$form = new SendMessageForm($sendMessage);
+$t->isnt($form->getDefault('subject'), 'Re:');
+$t->is($form->getDefault('body'), "");
